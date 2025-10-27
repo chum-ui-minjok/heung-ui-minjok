84cdcc79eefc4706379c9853ee933cddd4ba732a
LeeYoungwoo <leeduddn98@gmail.com>
2025-10-27 16:16:33 +0900

    [Feat] spring-server 초기 설정

---
+++ backend/spring-server/src/main/java/com/heungbuja/HeungbujaBeApplication.java
@@ -0,0 +1,13 @@
+package com.heungbuja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeungbujaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeungbujaBeApplication.class, args);
+	}
+
+}
