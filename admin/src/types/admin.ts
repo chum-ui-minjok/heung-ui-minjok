f1f57cb6130ea107af4655d74a30c226511d2d64
jayeunpark <jayeunpark0704@gmail.com>
2025-12-02 12:51:35 +0900

    [Fix] 빌드오류해결

--- admin/src/types/admin.ts
+++ admin/src/types/admin.ts
@@ -1,7 +1,9 @@
-export enum AdminRole {
-  SUPER_ADMIN = 'SUPER_ADMIN',
-  ADMIN = 'ADMIN',
-}
+export const AdminRole = {
+  SUPER_ADMIN: 'SUPER_ADMIN',
+  ADMIN: 'ADMIN',
+} as const;
+
+export type AdminRole = typeof AdminRole[keyof typeof AdminRole];
 
 export interface Admin {
   id: number;
